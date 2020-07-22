464124240bada2d504783a2253da36458c617e43
최순철 <scchoi@minigate.net>
2020-07-22 16:20:06 +0900

    Event Bus

---
+++ step05/bus/src/components/ByWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>{{ msg }}</div>
+</template>
+
+<script>
+import eventBus from "../eventBus";
+export default {
+  data() {
+    return {
+      msg: "",
+      count: 0,
+    };
+  },
+  destroyed() {
+    console.log("destroyed");
+  },
+  created() {
+    eventBus.$on("push-msg", (payload) => {
+      this.msg = payload;
+      this.count++;
+      console.log(this.count);
+    });
+  },
+};
+</script>
---
+++ step05/bus/src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <button v-on:click="clickBtn">HelloWorld</button>
+    <button v-on:click="changeComponent">Change Component</button>
+    <ByWorld v-if="isComponent"></ByWorld>
+  </div>
+</template>
+
+<script>
+import eventBus from "../eventBus";
+import ByWorld from "./ByWorld";
+
+export default {
+  name: "HelloWorld",
+  components: {
+    ByWorld,
+  },
+  data() {
+    return {
+      isComponent: true,
+    };
+  },
+  methods: {
+    clickBtn() {
+      eventBus.$emit("push-msg", "안녕");
+    },
+    changeComponent() {
+      this.isComponent = !this.isComponent;
+    },
+  },
+};
+</script>
